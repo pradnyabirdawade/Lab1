2752549d5a6456ee3e70556a88e0b19a21eaa894
pradnyabirdawade <166538166+pradnyabirdawade@users.noreply.github.com>
2025-02-21 18:42:07 +0530

    Add files via upload

---
+++ Pythonprog2.py
@@ -0,0 +1,11 @@
+Q2) Declare two variables and print that which variable is
+largest using ternary operators in python ?
+=
+
+num1 = 10
+num2 = 20
+largest = num1 if num1 > num2 else num2
+print(f"The largest number is: {largest}")
+
+OUTPUT :
+The largest number is: 20
---
+++ pythonprog1.py
@@ -0,0 +1,13 @@
+Q1) Calculate the multiplication and sum of two numbers  in Python ?
+
+num1 = 10
+num2 = 20
+sum_result = num1 + num2
+mul_result = num1 * num2
+
+print("Sum:", sum_result)
+print("Multiplication:", mul_result)
+
+OUTPUT : 
+Sum: 30
+Multiplication: 200
